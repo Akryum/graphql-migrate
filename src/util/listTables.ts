da9a41f3ce7d469f24005ea7154fbfc1a561bada
Guillaume Chau <alphadelta.fg@gmail.com>
2019-02-22 15:25:59 +0100

    fix: table duplicates

--- src/util/listTables.ts
+++ src/util/listTables.ts
@@ -28,7 +28,7 @@ const queries: any = {
   pg: (knex: Knex, schemaName: string) => ({
     sql: `select t.table_name, d.description from information_schema.tables t
     inner join pg_class c on c.relkind = 'r' and c.relname = t.table_name
-    left join pg_description d on d.objoid = c.oid
+    left join pg_description d on d.objoid = c.oid and d.objsubid = 0
     where t.table_schema = ? and t.table_catalog = ?`,
     bindings: [schemaName, knex.client.database()],
     output: (resp: any) => resp.rows.map((table: any) => ({ name: table.table_name, comment: table.description })),
